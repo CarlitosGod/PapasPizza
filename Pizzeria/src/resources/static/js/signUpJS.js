baf15ee264c2c409f0c0a6404cab9814b8dcef46
C Javier uC <101149813+CarlitosGod@users.noreply.github.com>
2022-09-22 20:18:30 -0500

    Un poco de JS
    Se hizo un JS que genera los pedidos bien melos

--- Pizzeria/src/resources/static/js/signUpJS.js
+++ Pizzeria/src/resources/static/js/signUpJS.js
@@ -1,32 +1,20 @@
+var i = 1;
 function sendData(pws) {
-    var x = document.getElementById("P1").value;
-    alert(x);
+    var val1 = document.getElementById("P1").value;
+    var val2 = document.getElementById("P2").value;
+    var val3 = document.getElementById("P3").value;
+    var val4 = document.getElementById("P4").value;
+    
+    // alert(val1);
+    // document.write("Usted pidio: Pan " + val1 + " con una porcion de " + val2 + ", con " + val3 + " y con " + val4);
+
+    addElemento("Pedido #"+i+": usted pidio " + val1 + " con " + val2 + ", " + val3 + " y " + val4)
+    i = i + 1;
 }
 
-function sha512(str) {
-    return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
-        return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
-    });
+function addElemento(texto) {
+    var capa = document.getElementById("capa");
+    var h1 = document.createElement("h2");
+    h1.innerHTML = texto;
+    capa.appendChild(h1);
 }
-
-function getPass(){
-    sha512($("#pass").val()).then(x => sendData(x));
-
-}
-
-/*
-function getData(){
-    $.ajax({
-        dataType: 'json',
-        url:"http://localhost:8081/api/Person/all",
-        type:'GET',
-        contentType:'application/json',
-        success:function(response) {
-            console.log(response);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-
-        }
-    });
-}
-*/
